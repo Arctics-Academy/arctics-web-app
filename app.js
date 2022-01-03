babc02bd6128cb83b32169fca4f69980175cd023
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-01-03 23:59:23 +0100

    Feat: Reformat to existing react & api structure

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.routes');
+var apiRouter = require('./routes/api.routes');
 
 var app = express();
 
@@ -12,9 +12,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+app.use('*', indexRouter);
+
 
 module.exports = app;
