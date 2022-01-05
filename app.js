dccfc6ae9a0fe8b22f1bf7795285c78a853d3765
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-01-05 01:00:45 +0100

    Feat: Add mongodb connection feature

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index.routes');
 var apiRouter = require('./routes/api.routes');
 
 var app = express();
+dotenv.config();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,8 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+databaseConfig();
+
 app.use('/api', apiRouter);
 app.use('*', indexRouter);
 
+
+async function databaseConfig() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+    }
+    catch(e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
 
 module.exports = app;
