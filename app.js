0f0f2c940a555239ab5182b15df59abbff3428ee
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-01-17 18:48:51 +0000

    Fix: Add cors middleware to verify cross address requests

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv');
+var cors = require('cors');
 var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index.routes');
@@ -16,6 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+const cors_whitelist = [ 'https://arctics.academy', 'https://www.arctics.academy' ];
+const cors_options = {
+    origin: (origin, callback) => {
+        if (cors_whitelist.indexOf[origin] !== -1) callback(true, null);
+        else new Error('not allowed by cors');
+    }
+}
+app.options('*', cors(cors_options));
 
 databaseConfig();
 
