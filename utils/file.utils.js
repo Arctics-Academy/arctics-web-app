dfde00775487c458621fce75d1695e565aa374ca
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-01-18 18:47:10 +0000

    Feat: Add read txt file to string function

--- utils/file.utils.js
+++ utils/file.utils.js
@@ -5,5 +5,8 @@ let read_file_to_base64 = (path) => {
     return fs.readFileSync(path, {encoding: 'base64'});
 }
 
+let read_file_to_string = (path) => {
+    return fs.readFileSync(path);
+}
 
-module.exports = { read_file_to_base64 }
+module.exports = { read_file_to_base64, read_file_to_string }
