d7d2f3d4430ab1afa0fa01405b244806d8ca2d3f
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-02-06 09:26:44 +0000

    Fix: Fix index router fault

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var path = require('path');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('*', function(req, res) {
   try {
-    res.status(200).sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    res.status(200).sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
   }
   catch(e) {
     console.debug(e);
-    res.sendStatus(404);
+    res.sendStatus(500);
   }
 });
 
