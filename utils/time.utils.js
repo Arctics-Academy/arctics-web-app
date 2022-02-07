06fb8b820bbf570f292a86a9f54ac2361d61dbbd
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-02-07 15:32:29 +0000

    Refactor: Unify coding style

--- utils/time.utils.js
+++ utils/time.utils.js
@@ -3,19 +3,19 @@ function currentTimeString() {
 
     // Date
     let str = String(now.getFullYear()) + "-";
-    str += __twoDigit(String(now.getMonth() + 1)) + "-";
-    str += __twoDigit(String(now.getDate())) + " ";
+    str += _twoDigit(String(now.getMonth() + 1)) + "-";
+    str += _twoDigit(String(now.getDate())) + " ";
     
     // Time
-    str += __twoDigit(String(now.getHours())) + ":";
-    str += __twoDigit(String(now.getMinutes())) + ":";
-    str += __twoDigit(String(now.getSeconds()));
+    str += _twoDigit(String(now.getHours())) + ":";
+    str += _twoDigit(String(now.getMinutes())) + ":";
+    str += _twoDigit(String(now.getSeconds()));
 
     return str;
 }
 
 
-function __twoDigit(str) {
+function _twoDigit(str) {
     if (str.length == 1) str = "0" + str;
     return str;
 }
