e49a25d7fcd3d05a523d57375de8d12750c74537
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-02-07 15:34:20 +0000

    Chore: Update consultant model

--- models/consultant.models.js
+++ models/consultant.models.js
@@ -7,7 +7,9 @@ const { MediaSchema } = require('./system.models');
 const ConsultantTransactionSchema = new mongoose.Schema({
     timestamp: { type: Date, required: true },
     content: { type: String, required: true },
-    amount: { type: Number, required: true }
+    amount: { type: Number, required: true },
+    balance: { type: Number, required: true },
+    withdrawn: { type: Number, required: true }
 }, { _id: false });
 
 const ConsultantNotificationSchema = new mongoose.Schema({
@@ -18,6 +20,12 @@ const ConsultantNotificationSchema = new mongoose.Schema({
     read: { type: Boolean, required: true, default: false }
 }, {_id: false });
 
+const ConsultantAnnouncementSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    timestamp: { type: Date, required: true },
+    read: { type: Boolean, required: true, default: false }
+}, { _id: false });
+
 const ConsultantSchema = new mongoose.Schema({
     id: { type: String, required: true, index: true, select: true, unique: true },
     profile: {
@@ -27,6 +35,7 @@ const ConsultantSchema = new mongoose.Schema({
         name: { type: String, required: true }, // 名字
         count: { type: Number, required: true, default: 0 }, // 已諮詢
         star: { type: Number }, // 星星
+
         // 基本資料
         price: { type: Number, required: true, default: 200 }, // 費用
         school: { type: String, required: true }, // 就讀&畢業
@@ -36,13 +45,16 @@ const ConsultantSchema = new mongoose.Schema({
         labels: { type: [String], required: true }, // 諮詢項目
         experiences: { type: String, required: true }, // 相關經歷&能力證明
         intro: { type: String, required: true }, // 個人簡介
+
         // 帳戶設定 (密碼不傳)
         email: { type: String, required: true }, // 電子信箱
         emailBackup: { type: String }, // 備援信箱
         mobile: { type: String, required: true }, // 手機號碼
         mobileBackup: { type: String }, // 備援號碼
+
         // 時間表
         timetable: { type: [[Number], [Number], [Number], [Number], [Number], [Number], [Number]], required: true, default: [[], [], [], [], [], [], []] },
+
         // 修改才出現的項目
         minor: { type: String }, // 雙主修&輔修
         emailVerified: { type: Boolean, required: true, default: false }, // 驗證信箱
@@ -52,14 +64,20 @@ const ConsultantSchema = new mongoose.Schema({
     },
     purse: {
         balance: { type: Number, required: true, default: 0 },
-        transactions: { type: [ConsultantTransactionSchema], required: true , default: []}
+        withdrawn: { type: Number, required: true, default: 0 },
+        transactions: { type: [ConsultantTransactionSchema], required: true , default: [] }
     },
     meetings: {
-        upcoming: { type: [String], required: true, default: []},
-        else: { type: [String], required: true, default: []}
+        future: { type: [String], required: true, default: [] },
+        past: { type: [String], required: true, default: [] },
+        canceled: { type: [String], required: true, default: [] }
+    },
+    announcements: {
+        unreadCount: { type: Number, required: true, default: 0 },
+        list: { type: [ConsultantAnnouncementSchema], required: true, default: [] }
     },
     notifications: {
-        count: { type: Number, required: true, default: 0 },
+        unreadCount: { type: Number, required: true, default: 0 },
         list: { type: [ConsultantNotificationSchema], required: true, default: [] }
     }
 }, { _id: false, collection: "Consultants" });
