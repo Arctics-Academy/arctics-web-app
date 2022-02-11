ec0583f7eca6fdb433fa482bbcafa0fb74429a9a
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-02-11 12:17:24 +0000

    Chore: Add time parsing methods to time util mod

--- utils/time.utils.js
+++ utils/time.utils.js
@@ -14,6 +14,36 @@ function currentTimeString() {
     return str;
 }
 
+function yearMonthToDatetimeRange(year, month) {
+    let start = new Date();
+    start.setFullYear(year);
+    start.setMonth(month);
+    start.setDate(1);
+    
+    let end = new Date();
+    if (month === 12) {
+        end.setFullYear(year+1);
+        end.setMonth(1);
+        end.setDate(1);
+    }
+    else {
+        end.setFullYear(year);
+        end.setMonth(month+1);
+        end.setDate(1);
+    }
+
+    return [start, end];
+}
+
+function previousMonth(year, month) {
+    if (month === 1) return [year-1, 12];
+    else return [year, month-1];
+}
+
+function nextMonth(year, month) {
+    if (month === 12) return [year+1, 1];
+    else return [year, month+1]
+}
 
 function _twoDigit(str) {
     if (str.length == 1) str = "0" + str;
@@ -21,4 +51,10 @@ function _twoDigit(str) {
 }
 
 
-module.exports = { currentTimeString };
+module.exports = 
+{
+    currentTimeString,
+    yearMonthToDatetimeRange,
+    previousMonth,
+    nextMonth
+};
