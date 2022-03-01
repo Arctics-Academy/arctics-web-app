5c2b36aeb2191b7012993f949d142a93b4c99f25
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-01 15:59:11 +0000

    fix: add cors whitelist

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var apiRouter = require('./routes/api.routes');
 var app = express();
 dotenv.config();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
