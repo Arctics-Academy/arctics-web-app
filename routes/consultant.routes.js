176742d3b379baea5f3dbc8125880344fcb843c6
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-03 12:17:22 +0000

    fix: fix catch empty file function

--- routes/consultant.routes.js
+++ routes/consultant.routes.js
@@ -163,6 +163,7 @@ router.post('/profile/student-id/update', StudentIdUploadMiddleware.single('stud
     try {
         if (!req.file) {
             res.status(500).json({ status: "error", message: "did not receive file"})
+            return
         }
         await consultantController.consultantAddStudentId(req.body.id, req.file)
         res.status(200).json({ status: "success", message: `upload student id scan for consultant ${req.body.id} successful` })
