eea5f5847546409cd6ded26fcfb6916980b939e5
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-03-05 03:33:53 +0000

    fix: fix cors

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var apiRouter = require('./routes/api.routes');
 var app = express();
 dotenv.config();
 
-app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,8 +28,7 @@ app.use(session({
 		httpOnly: true
 	}
 }))
-
-// app.use(csurf())
+app.use(cors())
 
 databaseConfig();
 
