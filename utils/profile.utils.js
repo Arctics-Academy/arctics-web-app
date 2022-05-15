b009395f07a4108299d351b238b938df3ce2f4a2
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-05-15 13:53:42 +0100

    fix: fix consultant filter fault

--- utils/profile.utils.js
+++ utils/profile.utils.js
@@ -1,13 +1,14 @@
 const castToStudentListConsultant = function(consultant) {
     let studentListConsultant = {
         consultantId: consultant.id,
-        photo: consultant.profile.photo,
+        photo: (consultant.profile.photo ? consultant.profile.photo : null),
         price: consultant.profile.price,
         school: consultant.profile.school,
         count: consultant.profile.count,
         labels: consultant.profile.labels,
         intro: consultant.profile.intro,
-        star: consultant.profile.star
+        star: (consultant.profile.star ? consultant.profile.star : null),
+        major: consultant.profile.major
     };
     return studentListConsultant;
 }
