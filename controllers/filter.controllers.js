2fb61425febc68a172219df250118a736e267b51
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-05-15 14:51:42 +0100

    test: add backend log script

--- controllers/filter.controllers.js
+++ controllers/filter.controllers.js
@@ -8,6 +8,10 @@ const { castToStudentListConsultant } = require('../utils/profile.utils')
 
 // Functions
 const filterConsultants = async (reqBody) => {
+
+    console.log("reqbody");
+    console.log(reqBody);
+    
     let mongoQuery = {};
     mongoQuery["profile.studentCardVerified"] = true;
     if (reqBody.query.major) {
@@ -19,6 +23,8 @@ const filterConsultants = async (reqBody) => {
     if (reqBody.query.field) {
         mongoQuery["profile.field"] ={ $in: reqBody.query.field };
     }
+    
+    console.log(mongoQuery);
 
     let rawResults = await ConsultantModel.find(mongoQuery).select("id profile");
 
